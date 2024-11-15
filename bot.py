0d311d33263dd67f037f1f60be8c09d6ec8139c4
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2024-11-14 23:11:29 -0300

    first commit

---
+++ bot.py
@@ -0,0 +1,13 @@
+print("Hello! What is your name?")
+name = "Samuel Ramos"
+print(f"Nice to meet you, {name}!")
+
+print("How old are you?")
+age = 24
+bot_age = 3
+age_difference = age - bot_age
+print(f"You are {age_difference} years older than me. I'm only {bot_age} years old!")
+
+print("What's your favorite color?")
+color = "Pink"
+print(f"Oh, {color} is a beautiful color!")
